a80fc88acbdb6b9d8f621b7f7d4edf2e83a5db9a
Sharad Shukla <sharadshukla@Laptop-von-Sharad.local>
2024-06-19 21:41:08 +0530

    logger

---
+++ lld_problems/logger/logger.py
@@ -0,0 +1,58 @@
+# Design a logger
+# Using Chain Responsibility principle
+
+from abc import ABC, abstractmethod
+class Logger(ABC):
+    def __init__(self, next_logger):
+        self.logger  = next_logger
+    @abstractmethod
+    def log(self,log_level):
+        if self.logger is not None:
+            self.logger.log(log_level)
+
+
+class InfoLogger(Logger):
+    def __init__(self, next_logger):
+        super().__init__(next_logger)
+
+    def log(self,log_level):
+        if log_level == 'info':
+            print("Info log .")
+        else:
+            print("not a info log .")
+            super().log(log_level)
+
+
+class DebugLogger(Logger):
+    def __init__(self, next_logger):
+        super().__init__(next_logger)
+
+    def log(self, log_level):
+        if log_level == 'debug':
+            print("Debug log .")
+        else:
+            print("not a debug log .")
+            super().log(log_level)
+
+class ErrorLogger(Logger):
+    def __init__(self, next_logger):
+        super().__init__(next_logger)
+
+    def log(self, log_level):
+        if log_level == 'error':
+            print("ERROR log .")
+        else:
+            print("not a error log .")
+            super().log(log_level)
+
+class LoggerHandler():
+    def main(self):
+        log_obj = InfoLogger(DebugLogger(ErrorLogger(None)))
+        log_obj.log('error')
+
+if __name__ == '__main__':
+    LoggerHandler().main()
+
+
+
+
